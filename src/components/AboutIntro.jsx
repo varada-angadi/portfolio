228c4201c4130cc3b992b14b02ad23646e52a953
Varada2806 <107529891+Varada2806@users.noreply.github.com>
2025-10-08 13:47:14 +0530

    Updated AboutIntro

--- src/components/AboutIntro.jsx
+++ src/components/AboutIntro.jsx
@@ -12,7 +12,7 @@ export default function AboutIntro() {
   // Scroll progress tracker for More about..
   const { scrollYProgress } = useScroll({
     target: ref,
-    offset: ["start end", "end start"], 
+    offset: ["start end", "end start"],
   });
 
   // Based on scroll progress opacity change
@@ -36,7 +36,7 @@ export default function AboutIntro() {
       const sectionHeight = rect.height;
 
       const scrollProgress = Math.max(0, Math.min(
-          1,(windowHeight - sectionTop) / (sectionHeight + windowHeight)));
+        1, (windowHeight - sectionTop) / (sectionHeight + windowHeight)));
 
       setProgress(scrollProgress);
     };
@@ -58,52 +58,64 @@ export default function AboutIntro() {
   return (
     <>
       {/* Project section heading */}
-        <section ref={ref} className="relative h-[120vh]">
-          <motion.div style={{ opacity, scale }} className="sticky top-0 h-screen flex items-center justify-center">
-            <p className="font-bold font-poppins text-5xl sm:text-6xl md:text-7xl text-center leading-normal">
-              More About <br />
-              <span className="tracking-[5px] sm:tracking-[8px] md:tracking-[10px]">VARADA ANGADI</span>
-            </p>
-          </motion.div>
-        </section>
+      <section ref={ref} className="relative h-[120vh]">
+        <motion.div style={{ opacity, scale }} className="sticky top-0 h-screen flex items-center justify-center">
+          <p className="font-bold font-poppins text-5xl sm:text-6xl md:text-7xl text-center leading-normal">
+            More About <br />
+            <span className="tracking-[5px] sm:tracking-[8px] md:tracking-[10px]">VARADA ANGADI</span>
+          </p>
+        </motion.div>
+      </section>
 
       {/* About Section */}
       <section ref={sectionRef} className="w-full h-[calc(100vh+20px)] flex items-center justify-center z-20 px-4 mb-32 md:mb-16 lg:mb-6">
         <div className="flex flex-col md:flex-row w-full max-w-6xl items-center justify-between gap-10">
-    
-        {/* Left Side Image */}
-        <div style={{ opacity: contentOpacity, transform: `scale(${imageScale})`, transition: "transform 0.3s ease-out",}}
-        className="w-full md:w-1/2 flex justify-center">
-          <img src="/images/img.png" alt="Varada Angadi" className="w-[14rem] sm:w-[18rem] md:w-[20rem] h-auto object-cover rounded-xl shadow-2xl"/>
-        </div>
 
-        {/* Right Side About Text */}
-        <div style={{opacity: contentOpacity, transform: `translateY(${textTranslate}px)`, transition: "transform 0.3s ease-out",}}
-        className="w-full md:w-1/2 max-w-lg text-gray-100 text-center md:text-left">
-          <h3 className="text-2xl sm:text-3xl md:text-[36px] text-white leading-tight font-poppins mb-4">
-            About Me
-          </h3>
-
-          <div className="text-[16px] sm:text-[18px] leading-relaxed font-poppins tracking-[1px] font-thin space-y-4">
-            <p>Hi, I’m Varada, a software developer with technical interests in Java, SQL, and operating systems.</p>
-            <p>I’m drawn to work that balances logic with emotion. But what excites me the most is the process of solving real problems and the immense joy of seeing it all come together.</p>
-            <p>I chase calm in organizing, cooking, and caring for my plants. I’m not perfect at painting, photography or embroidery but I love the process because creating, in any form, makes life lighter.</p>
+          {/* Left Side Image */}
+          <div style={{ opacity: contentOpacity, transform: `scale(${imageScale})`, transition: "transform 0.3s ease-out", }}
+            className="w-full md:w-1/2 flex justify-center">
+            <img src="/images/img.png" alt="Varada Angadi" className="w-[14rem] sm:w-[18rem] md:w-[20rem] h-auto object-cover rounded-xl shadow-2xl" />
           </div>
 
-          {/* Button */}
-          <div className="mt-6 flex justify-center md:justify-start mb-6">
-            <button className="w-[180px] h-[50px] border-2 rounded-[20px] flex items-center justify-center"
-              style={{ backgroundColor: "#070707", borderColor: "white" }}>
-              <a
-                href=""
-                className="text-[#0C6B2C] text-center font-bold text-[16px] sm:text-[18px] font-poppins hover:text-[#8DA563] transition-colors"
-                target="_blank"
-                rel="noopener noreferrer">
-                Beyond the Code
-              </a>
-            </button>
+          {/* Right Side About Text */}
+          <div style={{ opacity: contentOpacity, transform: `translateY(${textTranslate}px)`, transition: "transform 0.3s ease-out", }}
+            className="w-full md:w-1/2 max-w-lg text-gray-100 text-center md:text-left">
+            <h3 className="text-2xl sm:text-3xl md:text-[36px] text-white leading-tight font-poppins mb-4">
+              About Me
+            </h3>
+
+            <div className="text-[16px] sm:text-[18px] leading-relaxed font-poppins tracking-[1px] font-thin space-y-4">
+              <p>Hi, I’m Varada, a software developer specializing in Java, SQL, and development, 
+                with hands-on experience in building scalable, efficient, and maintainable software systems.</p>
+
+              <p>
+                {/* I’m drawn to work that balances logic with emotion.  */}
+                My work combines precision, structure, and creativity to bring ideas to life effectively.
+                But what excites me the most is the process of solving real problems and the immense joy of seeing it all come together.</p>
+
+              {/* <p>I chase calm in organizing, cooking, and caring for my plants. While I experiment with creative pursuits like 
+                painting, photography, and embroidery, I love the process because creating, in any form, makes life lighter.</p> */}
+
+                <p>Outside of coding, I enjoy organizing, cooking, and caring for my plants. 
+                  Creative hobbies help me maintain focus and a process-driven mindset.</p>
+            </div>
+
+
+
+            {/* Button */}
+            <div className="mt-6 flex justify-center md:justify-start mb-6">
+              <button className="w-[180px] h-[50px] border-2 rounded-[20px] flex items-center justify-center"
+                style={{ backgroundColor: "#070707", borderColor: "white" }}>
+                <a
+                  href=""
+                  className="text-[#0C6B2C] text-center font-bold text-[16px] sm:text-[18px] font-poppins hover:text-[#8DA563] transition-colors"
+                  target="_blank"
+                  rel="noopener noreferrer">
+                  Beyond the Code
+                </a>
+              </button>
+            </div>
           </div>
-        </div>
         </div>
       </section>
     </>
